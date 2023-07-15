6a26fffb50c25d1633c6e266b8c1be1a319f54fd
c0nstantin1990 <118696575+c0nstantin1990@users.noreply.github.com>
2023-07-15 11:27:24 -0400

    fixed belongsToMany

--- models/index.js
+++ models/index.js
@@ -12,17 +12,17 @@ Product.belongsTo(Category, {
 Category.hasMany(Product, {
   foreignKey: "category_id",
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-
-  foreignKey: "tag_id",
+  foreignKey: "product_id",
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "product_id",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
